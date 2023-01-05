d2b3ef864d1b586ac0fba8714749253f39afa8c4
RenatoLPSjr <renato.lop.jr@gmail.com>
2023-01-04 21:34:01 -0300

    Criando pagina HTML

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get("/" ,(req, res)=> {
-
-    res.render("index",{
-
-    })
+    res.render("index")
 });
+
+app.get("/perguntar", (req, res)=>{
+    res.render("perguntar");
+})
+
 
 
 app.listen(8080,()=>{
